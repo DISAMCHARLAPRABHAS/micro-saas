cb96842203c24b663c5b1ece328c8048ae9f2215
Prabhas Naidu <prabhasnaidu188@gmail.com>
2025-09-12 11:40:44 +0000

    first commit

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -5,7 +5,7 @@ export function AppLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
+      viewBox="0 0 64 64"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
